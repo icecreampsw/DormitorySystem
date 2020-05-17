cba8def293b1b77ff7e4583526007db3bd942fbf
ธมนณัฎฐ์ พูลสวัสดิ์ 6109650611 <58017522+icecreampsw@users.noreply.github.com>
2020-05-18 03:21:43 +0700

    Add files via upload

---
+++ Yes.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class="header">
+<navigation>HEADER</navigation>
+</header>
+<section class="main">
+
+</section>
+<footer class="footer">
+<navigation>FOOTER</navigation>
+</footer>
+<style>
+.header {
+position: absolute;
+top: 0;
+left: 0;
+right: 0;
+height: 50px;
+line-height: 50px;
+background-color: #235680;
+color:white;
+text-align: center;
+width:1482.5px;
+ height:500px;
+}
+.footer {
+position: absolute;
+bottom: 0;
+left: 0;
+right: 0;
+height: 50px;
+line-height: 50px;
+background-color: red;
+color:white;
+font-size: 20px;
+text-align: center;
+}
+.main {
+position: absolute;
+top: 50px;
+bottom: 0;
+left: 0;
+right: 0;
+line-height: 50px;
+background-image: url("Dorm.jpg") ;
+background-repeat:no-repeat;
+overflow: auto;
+}
+</style> 
+</body>
+</html>
